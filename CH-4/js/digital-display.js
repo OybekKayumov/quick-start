4ce92e77175ae5a02da1d22cdc07623eadcc9f08
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-02 14:49:55 +0500

    add js clock digital

--- CH-4/js/digital-display.js
+++ CH-4/js/digital-display.js
@@ -1,9 +1,19 @@
 const DigitalDisplay = function (props) {
+
+  let hourStyle = {
+    margin: -20,
+    border: '1px solid grey',
+    width: '60%',
+    height: 30,
+    backgroundColor: 'orange'
+  };
+
   return React.createElement (
     'div',
     null,
-    props.time
-  )
+    props.time,
+    React.createElement('div', { style: hourStyle }),
+  )  
 }
 
 export default DigitalDisplay
